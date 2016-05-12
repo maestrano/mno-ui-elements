fb02cbbf63f73cf518f5bab6833f12977a6f2660
Clement Berti <clementberti@yahoo.fr>
2016-05-12 16:00:55 +1000

    Still fixing component modules

--- dist/mno-ui-elements.js
+++ dist/mno-ui-elements.js
@@ -2,22 +2,38 @@
 
 
 }());
+/*                                                                          **
+**                      MAESTRANO MNO-UI-ELEMENTS LIBRARY                   **
+**                                                                          */
+//***************************************************************************/
+//* Create all modules and define dependencies.
+//*************************************************************************/
+angular.module('mnoUiElements',
+    [
+        'mnoUiElements.config',
+        'mnoUiElements.services',
+        'mnoUiElements.components'
+    ]);
+
+
 // Create all modules and define dependencies to make sure they exist
 // and are loaded in the correct order to satisfy dependency injection
 // before all nested files are concatenated by Grunt
 
-// Config
+/*
+** CONFIG
+*/
 angular.module('mnoUiElements.config', [])
     .value('mnoUiElements.config', {
         debug: true
     });
 
-// Modules
-angular.module('mnoUiElements.core', []);
-angular.module('mnoUiElements.services', []);
-angular.module('mnoUiElements',
+/*
+** COMPONENTS
+*/
+angular.module('mnoUiElements.components',
     [
-        'mnoUiElements.config',
-        'mnoUiElements.services',
-        'mnoUiElements.core'
+        'mnoUiElements.components.loading'
     ]);
+
+angular.module('mnoUiElements.services', []);
--- src/mnoUiElements.js
+++ src/mnoUiElements.js
@@ -1,22 +1,35 @@
-// Create all modules and define dependencies to make sure they exist
-// and are loaded in the correct order to satisfy dependency injection
-// before all nested files are concatenated by Grunt
-
-// Config
-angular.module('mnoUiElements.config', [])
-    .value('mnoUiElements.config', {
-        debug: true
-    });
-
-// Modules
-angular.module('mnoUiElements.components',
-    [
-        'mnoUiElements.components.loading'
-    ]);
-angular.module('mnoUiElements.services', []);
+/*                                                                          **
+**                      MAESTRANO MNO-UI-ELEMENTS LIBRARY                   **
+**                                                                          */
+//***************************************************************************/
+//* Create all modules and define dependencies.
+//*************************************************************************/
 angular.module('mnoUiElements',
     [
         'mnoUiElements.config',
         'mnoUiElements.services',
         'mnoUiElements.components'
     ]);
+
+
+// Create all modules and define dependencies to make sure they exist
+// and are loaded in the correct order to satisfy dependency injection
+// before all nested files are concatenated by Grunt
+
+/*
+** CONFIG
+*/
+angular.module('mnoUiElements.config', [])
+    .value('mnoUiElements.config', {
+        debug: true
+    });
+
+/*
+** COMPONENTS
+*/
+angular.module('mnoUiElements.components',
+    [
+        'mnoUiElements.components.loading'
+    ]);
+
+angular.module('mnoUiElements.services', []);
