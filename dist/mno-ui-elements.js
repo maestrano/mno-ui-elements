14107d7c59633613562186033059664644274152
Clement <clementberti@yahoo.fr>
2016-11-10 19:06:20 +1100

    Updated the admin component

--- dist/mno-ui-elements.js
+++ dist/mno-ui-elements.js
@@ -11,7 +11,7 @@ angular.module('mnoUiElements', [
 }());
 (function() {
   angular.module('admin', []).component('mnoAdmin', {
-    template:'<div id=sidebar-wrapper ng-class="{\'reduced\': $ctrl.reduced}"><ul class=sidebar><li class=sidebar-main><a ng-click=$ctrl.reduceSidebar()><img class="sidebar-logo visible-sm visible-xs" src={{$ctrl.logo}}> <span class="menu-icon glyphicon glyphicon-transfer hidden-sm hidden-xs"></span></a></li><div ng-transclude=navigation></div></ul><div class=sidebar-footer></div></div><div id=content-wrapper ng-class="{\'open\': $ctrl.toggled, \'reduced\': $ctrl.reduced}"><header class=header><a class="header-sidebar-toggle visible-sm visible-xs" ng-click=$ctrl.toggleSidebar()><div id=hamburger-toggle ng-class="{\'open\': $ctrl.toggled}"><span></span> <span></span> <span></span></div></a><div ng-transclude=header></div></header><div class=page-content ng-transclude=content></div><footer><div ng-transclude=footer></div></footer></div>',
+    template:'<div id="sidebar-wrapper" ng-class="{\'reduced\': $ctrl.reduced}"><ul class="sidebar"><li class="sidebar-main"><a ng-click="$ctrl.reduceSidebar()"><img class="sidebar-logo visible-sm visible-xs" src="{{$ctrl.logo}}"> <span class="menu-icon glyphicon glyphicon-transfer hidden-sm hidden-xs"></span></a></li><div ng-transclude="navigation"></div></ul><div class="sidebar-footer"></div></div><div id="content-wrapper" ng-class="{\'open\': $ctrl.toggled, \'reduced\': $ctrl.reduced}"><header class="header"><a class="header-sidebar-toggle visible-sm visible-xs" ng-click="$ctrl.toggleSidebar()"><div id="hamburger-toggle" ng-class="{\'open\': $ctrl.toggled}"><span></span> <span></span> <span></span></div></a><div ng-transclude="header"></div></header><div class="page-content" ng-transclude="content"></div><footer ng-if="!$ctrl.hideFooter"><div ng-transclude="footer"></div></footer></div>',
     transclude: {
       header: 'mnoAdminHeader',
       navigation: 'mnoAdminNav',
@@ -19,7 +19,8 @@ angular.module('mnoUiElements', [
       footer: 'mnoAdminFooter'
     },
     bindings: {
-      logo: '@'
+      logo: '@',
+      hideFooter: '='
     },
     controller: ["$scope", function($scope) {
       var vm;
@@ -41,7 +42,7 @@ angular.module('mnoUiElements', [
       }), function(newValue) {
         if (newValue) {
           vm.toggled = false;
-          return vm.reduced = window.innerWidth > 992 ? true : false;
+          return vm.reduced = window.innerWidth > 992;
         }
       });
       window.onresize = function() {
