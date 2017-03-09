86a1cce2c65784eaca23a8bee8d70b8bda1473c2
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2017-03-10 09:50:14 +1100

    Build

--- dist/mno-ui-elements.js
+++ dist/mno-ui-elements.js
@@ -52,13 +52,6 @@ angular.module('mnoUiElements', [
 }).call(this);
 
 (function() {
-  angular.module('mnoUiElements').component('mnoLoadingEllipsis', {
-    template: '<div class="mno-three-bounce">\n  <div class="mno-bounce1"></div>\n  <div class="mno-bounce2"></div>\n  <div class="mno-bounce3"></div>\n</div>'
-  });
-
-}).call(this);
-
-(function() {
   angular.module('mnoUiElements').component('mnoKpi', {
     template:'<div><div class="visual"><i class="fa" ng-class="vm.icon || \'fa-file-text-o\'"></i></div><div class="details"><div class="loader" ng-show="vm.loading" aria-hidden="true"><i class="fa fa-2x fa-spin fa-refresh"></i></div><div class="body" ng-hide="vm.loading">{{vm.unit}} {{vm.value}}</div><div class="desc">{{vm.description}}</div></div><div ng-if="vm.mnoHref || vm.mnoUiSref || vm.mnoClick" class="more"><a ng-if="vm.mnoHref" ng-href="{{vm.mnoHref}}">{{vm.linkText || "View more"}} <i class="fa fa-arrow-circle-o-right"></i></a> <a ng-if="vm.mnoUiSref" ui-sref="{{vm.mnoUiSref}}">{{vm.linkText || "View more"}} <i class="fa fa-arrow-circle-o-right"></i></a> <a ng-if="vm.mnoClick" ng-click="vm.mnoClick()">{{vm.linkText || "View more"}} <i class="fa fa-arrow-circle-o-right"></i></a></div></div>',
     transclude: true,
@@ -74,70 +67,17 @@ angular.module('mnoUiElements', [
       mnoUiSref: '@?',
       linkText: '@?'
     },
-    controller: ["$scope", function($scope) {
+    controller: function() {
       var vm;
       vm = this;
-      vm.$onInit = function() {};
-    }]
+    }
   });
 
 }).call(this);
 
 (function() {
-  angular.module('mnoUiElements').component('mnoPassword', {
-    template:'<div ng-if="$ctrl.verifyOldPassword" class="row"><div class="col-md-6"><label>{{$ctrl.currentHeading || (\'devpl.view.settings.password.current_password\' | translate)}}</label> <input type="password" name="current_password" ng-model="$ctrl.data.current_password" <!--ng-change="$ctrl.password.resetServerErrors()" --> class="form-control" required></div></div><div class="row top-buffer-1"><div class="col-md-6"><label>{{$ctrl.newHeading || (\'devpl.view.settings.password.new_password\' | translate)}}</label> <input type="password" name="password" ng-model="$ctrl.data.password" ng-change="$ctrl.password.check()" class="form-control" required><div class="help-block"><ul class="col-sm-6 list-unstyled"><li><mno-validation valid="$ctrl.password.hasEightChars" message="{{\'devpl.view.settings.password.rules.length\' | translate}}"></mno-validation></li><li><mno-validation valid="$ctrl.password.hasOneNumber" message="{{\'devpl.view.settings.password.rules.number\' | translate}}"></mno-validation></li></ul><ul class="col-sm-6 list-unstyled"><li><mno-validation valid="$ctrl.password.hasOneUpper" message="{{\'devpl.view.settings.password.rules.uppercase\' | translate}}"></mno-validation></li><li><mno-validation valid="$ctrl.password.hasOneLower" message="{{\'devpl.view.settings.password.rules.lowercase\' | translate}}"></mno-validation></li></ul></div></div><div ng-if="$ctrl.confirmNewPassword" class="col-md-6"><label>{{$ctrl.confirmHeading || (\'devpl.view.settings.password.confirm_password\' | translate)}}</label> <input type="password" name="password_confirmation" ng-model="$ctrl.data.password_confirmation" ng-change="$ctrl.password.check()" class="form-control" required><div class="help-block"><ul class="col-sm-6 list-unstyled"><li><mno-validation valid="$ctrl.password.isSamePassword" message="{{\'devpl.view.settings.password.rules.same_password\' | translate}}"></mno-validation></li></ul></div></div></div>',
-    bindings: {
-      form: '=',
-      data: '=',
-      verifyOldPassword: '=?',
-      confirmNewPassword: '=?',
-      currentHeading: '@?',
-      newHeading: '@?',
-      confirmHeading: '@?'
-    },
-    controller: function() {
-      var vm;
-      vm = this;
-      if (!vm.verifyOldPassword) {
-        vm.verifyOldPassword = false;
-      }
-      if (!vm.confirmNewPassword) {
-        vm.confirmNewPassword = false;
-      }
-      vm.password = {
-        hasEightChars: false,
-        hasOneNumber: false,
-        hasOneUpper: false,
-        hasOneLower: false,
-        isSamePassword: false,
-        check: function() {
-          var passwordValidity;
-          vm.password.hasEightChars = (vm.data.password != null) && (vm.data.password.length >= 8);
-          vm.password.hasOneNumber = false;
-          vm.password.hasOneUpper = false;
-          vm.password.hasOneLower = false;
-          if (angular.isString(vm.data.password)) {
-            _.forEach(vm.data.password.split(""), function(letter) {
-              if (Number.isInteger(parseInt(letter))) {
-                vm.password.hasOneNumber = true;
-              }
-              if (letter === letter.toUpperCase() && letter !== letter.toLowerCase() && !parseInt(letter)) {
-                vm.password.hasOneUpper = true;
-              }
-              if (letter === letter.toLowerCase() && letter !== letter.toUpperCase() && !parseInt(letter)) {
-                return vm.password.hasOneLower = true;
-              }
-            });
-            passwordValidity = vm.password.hasEightChars && vm.password.hasOneNumber && vm.password.hasOneUpper && vm.password.hasOneLower;
-            vm.form.password.$setValidity("password", passwordValidity);
-          }
-          if (vm.confirmNewPassword) {
-            vm.password.isSamePassword = !!vm.data.password && !!vm.data.password_confirmation && vm.data.password === vm.data.password_confirmation;
-            return vm.form.password_confirmation.$setValidity("password_confirmation", vm.password.isSamePassword);
-          }
-        }
-      };
-    }
+  angular.module('mnoUiElements').component('mnoLoadingEllipsis', {
+    template: '<div class="mno-three-bounce">\n  <div class="mno-bounce1"></div>\n  <div class="mno-bounce2"></div>\n  <div class="mno-bounce3"></div>\n</div>'
   });
 
 }).call(this);
